8a8d77684c907814b37c77935d50c8fe1c2236b9
Your Name <you@example.com>
2026-09-17 04:49:41 +0200

    init

--- server.js
+++ server.js
@@ -11,8 +11,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/**
+  * Routes
+  */
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+
+app.get('/organizations', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
 });
 
 app.listen(PORT, () => {
