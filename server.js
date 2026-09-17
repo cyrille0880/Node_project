2aac77cd7fe077ed262e1cb5d15a81323024a9d8
Your Name <you@example.com>
2026-09-17 04:15:21 +0200

    init

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 
 const NODE_ENV = 'production';
 const PORT = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
